a60e8e46962827e6110fbefbfae55c58d4be9037
SerPan23 <igracom@yandex.ru>
2020-06-11 16:11:21 +0300

    u

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $('.header').height($(window).height());
 });
 
-$('.nav-link').on('click', function () {
+$('.navbar-nav>li>a').on('click', function () {
     $('.navbar-collapse').collapse('hide');
 });
