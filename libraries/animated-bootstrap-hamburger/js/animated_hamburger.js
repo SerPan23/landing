6e0743cfee79020e4af5b36b78f3a33baf7cbe01
SerPan23 <igracom@yandex.ru>
2020-06-19 20:52:58 +0300

    add_almbb

--- libraries/animated-bootstrap-hamburger/js/animated_hamburger.js
+++
@@ -1,9 +0,0 @@
-$('.navbar-collapse').on('click', function () {
-    $('.navbar-collapse').collapse('hide');
-    $('.navbar-toggler').removeClass("active");
-});
-$(document).ready(function () {
-    $(".navbar-toggler").on("click", function () {
-        $(this).toggleClass("active");
-    });
-});
